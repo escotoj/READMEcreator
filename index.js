a917857ab31c73d0e85fc0b221cc89602ffead96
escotoj <j.escoto14@gmail.com>
2023-04-04 19:08:58 -0700

    added gitignore and worked on cleaning index

---
+++ index.js
@@ -0,0 +1,55 @@
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+const q = [
+    {
+      type: 'input',
+      message: 'What is the title of the project?',
+      name: 'titleofProject',
+    },
+    {
+      type: 'input',
+      message: 'Please descrive your project and its purpose?',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'what are the steps to install this app',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Re-enter password to confirm:',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Please list contributors to this app to give credits',
+      name: 'credits',
+    },
+    {
+      type: 'input',
+      message: 'Please enter name of licence to be used',
+      name: 'licence',
+    },
+    {
+      type: 'input',
+      message: 'What are the instructions to run a test',
+      name: 'test',
+    },
+    {
+      type: 'input',
+      message: 'Please type in your GitHub username',
+      name: 'github',
+    },{
+      type: 'list',
+      message: 'Example of how to use you project',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'List the features in this project',
+      name: 'features',
+    },
+  ];
+
