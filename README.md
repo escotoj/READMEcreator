ce60edc25a0d28f52e195e59742f8f2b23850574
escotoj <j.escoto14@gmail.com>
2023-04-04 20:48:53 -0700

    added a function using temolate literals ot inject answers to README

---
+++ README.md
@@ -0,0 +1,24 @@
+# undefined
+  undefined
+  ## Description
+  to gain max rizz for the add major   potential at gains
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#credits)
+  - [License](#license)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Installation
+  well use js and node to achicve this 
+  ## Usage
+  no password
+  undefined
+  ## Contributing
+  jso esco
+  ## Tests
+  undefined
+  ## Questions
+  For more questions please contact below: 
+  Email:undefined 
+  GitHub: jose.
--- index.js
+++ index.js
@@ -19,11 +19,6 @@ inquirer.prompt([
     },
     {
       type: 'input',
-      message: 'Re-enter password to confirm:',
-      name: 'usage',
-    },
-    {
-      type: 'input',
       message: 'Please list contributors to this app to give credits',
       name: 'credits',
     },
@@ -41,6 +36,11 @@ inquirer.prompt([
       type: 'input',
       message: 'Please type in your GitHub username',
       name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'Re-enter password to confirm:',
+      name: 'email',
     },{
       type: 'list',
       message: 'Example of how to use you project',
@@ -58,6 +58,34 @@ inquirer.prompt([
         error
     );
 });
+
+// funtion that takes the data parameter to inject the answers in proper area using back ticks (template literals)
+function template(data) {
+  return `# ${data.titleofProject}
+  ${data.license}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#credits)
+  - [License](#license)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ${data.license}
+  ## Contributing
+  ${data.credits}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  For more questions please contact below: 
+  Email:${data.email} 
+  GitHub: ${data.github}.`;
+};
 
 // .then((answers) => {
 //   console.log(answers);
